80044649b2d3731e40f96bd6df9230ba525d1409
HernandoPinzon <hernandonunez13@gmail.com>
2025-06-03 11:33:53 -0500

    añadir variable production

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/my-web-component.jsx'),
-      name: 'MyWebComponent',
+      entry: path.resolve(__dirname, "src/my-web-component.jsx"),
+      name: "MyWebComponent",
       fileName: (format) => `my-web-component.${format}.js`,
-      formats: ['es'],
+      formats: ["es"],
     },
     rollupOptions: {
       external: [],
-    }
-  }
-})
+    },
+  },
+});
